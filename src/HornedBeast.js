196a75db9c059fac05a7b670f631a42cedaece59
thaerbraizat <thaerbraizat13@gmail.com>
2021-06-21 15:20:11 +0300

    lab2

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react'
 
 class HornedBeast extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            vote:0
+        }
+            
+        
+    }
+    changeVote=()=>{
     
+        this.setState(
+            {
+                vote:this.state.vote+1
 
-    render() {
+            })
+    }
+
+    render()
+     {
+         console.log(this.state.vote);
         return (
             <div>
                 <h2>{this.props.title}</h2>
                
-                <img src={this.props.image_url} alt={this.props.title}></img>
+                <img onClick={this.changeVote} src={this.props.image_url} alt={this.props.title}></img>
 
                 <p>{this.props.description}</p>
+                <p>{this.state.vote} <span style={{color: "red"}}>&#10084;</span></p>
             </div>
+            
         )
     }
 }
